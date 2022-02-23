30ec183452d89ac5e699f6b4cf40646a12b139e5
Julian <julianmartinez_93@hotmail.com>
2022-02-23 14:23:41 -0300

    casi terminado triangulo error en la concadenacion de la circunferencia

--- Figuras.js
+++ Figuras.js
@@ -19,3 +19,35 @@ function calcularAreaCuadrado(){
 }
 
 //Cuadrado fin
+
+//Triangulo
+
+
+
+let lado1 = document.getElementById("lado1");
+let lado2 = document.getElementById("lado2"); 
+let base = document.getElementById("base");
+let resultadotriangulo1 = document.getElementById("resultadotriangulo1");
+let lado1x = Number(lado1.value)
+let lado2x = Number(lado2.value)
+let basex = Number(base.value)
+
+function perimetrotriangulo(l1,l2,b){
+    return l1 + l2 + b;
+}
+function calcularPerimetroTriangulo(){
+    return resultadotriangulo1.innerHTML = perimetrotriangulo(lado1x,lado2x,basex) + "cm";
+}
+
+let base2 = document.getElementById("base2"); 
+let altura = document.getElementById("altura"); 
+let resultadotriangulo2 = document.getElementById("resultadotriangulo2");
+
+function areatriangulo(b,a){
+    return (b*a)/2
+}
+function calcularAreaTriangulo(){
+    return resultadotriangulo2.innerHTML = areatriangulo(base2.value, altura.value) + "m²"
+}
+//Triangulo fin
+
